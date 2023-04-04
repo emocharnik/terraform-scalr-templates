03e629b49c320f493ec9e712497ff595fd388b54
Edgar Mocharnyk <e.mocharnyk@scalr.com>
2023-04-04 17:18:47 +0300

    added examples

---
+++ examples/main.tf
@@ -0,0 +1,52 @@
+terraform {
+    required_providers {
+        scalr = {
+            source = "registry.scalr.io/scalr/scalr"
+            version= "~> 1.0.0"
+        }
+    }
+}
+
+variable "provider_name" {
+  type = string
+}
+
+variable "environment_name" {
+  type = string
+}
+
+data "scalr_vcs_provider" test {
+  name = var.provider_name
+}
+
+resource "scalr_environment" test {
+  name = "env-name"
+}
+
+resource "scalr_workspace" "dev" {
+  name            = "dev"
+  environment_id  = scalr_environment.test.id
+  vcs_provider_id = data.scalr_vcs_provider.test.id
+
+  working_directory = "app"
+
+  vcs_repo {
+      identifier          = "emocharnik/terraform-scalr-templates"
+      branch              = "main"
+      trigger_prefixes    = ["app", "variables/dev"]
+  }
+}
+
+resource "scalr_workspace" "prod" {
+  name            = "prod"
+  environment_id  = scalr_environment.test.id
+  vcs_provider_id = data.scalr_vcs_provider.test.id
+
+  working_directory = "app"
+
+  vcs_repo {
+      identifier          = "emocharnik/terraform-scalr-templates"
+      branch              = "main"
+      trigger_prefixes    = ["app", "variables/prod"]
+  }
+}
